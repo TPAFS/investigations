fe4abdeb896f9b29070607f4d0c62f1f217ab763
mike.gartner <mgartner112@gmail.com>
2023-03-04 21:58:08 -0500

    Update plot styling defaults

--- src/utils/plot.py
+++ src/utils/plot.py
@@ -1,11 +1,24 @@
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 
-def plot_bar(bar_names: list[str], bar_vals: list[float],
-             title = None, val_axis_label=None, cat_axis_label= None,
-             save_path: str=None, show=True):
-    """Draw and optionally show and save a bar chart via matplotlib.
-    """
+from cycler import cycler
+
+import typing as t
+
+plt.style.use('ggplot')
+mpl.rcParams['axes.prop_cycle'] = cycler(color=['#8f5e77', '#62aebf', '#E24A33', '#467821', '#348ABD', '#7A68A6', '#A60628', '#CF4457', '#188487', 'tab:orange'])
+
+def plot_bar(
+    bar_names: list[str],
+    bar_vals: list[float],
+    title: t.Optional[str] = None,
+    val_axis_label: t.Optional[str] = None,
+    cat_axis_label: t.Optional[str] = None,
+    save_path: t.Optional[str] = None,
+    show: bool = True,
+) -> None:
+    """Draw and optionally show and save a bar chart."""
     fig, ax = plt.subplots()
     x = np.arange(1, len(bar_names) + 1)
     ax.bar(x, height=bar_vals, tick_label=bar_names, log=False)
@@ -21,3 +34,22 @@ def plot_bar(bar_names: list[str], bar_vals: list[float],
     if show:
         plt.show()
     return None
+
+
+def plot_pie(
+    slice_names: list[str],
+    slice_vals: list[float],
+    title: t.Optional[str] = None,
+    save_path: t.Optional[str] = None,
+    show: bool = True,
+) -> None:
+    """Drawn and optionally show and save a pie chart."""
+    fig, ax = plt.subplots()
+    ax.pie(slice_vals, labels=slice_names)
+    if title:
+        ax.set_title(title)
+    if save_path:
+        plt.savefig(save_path)
+    if show:
+        plt.show()
+    return None  
